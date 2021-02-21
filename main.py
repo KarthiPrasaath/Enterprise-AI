6c199eb1d1fd65739ff3240f8b3dffbc1cb6c529
Karthi Prasaath E <78941446+KarthiPrasaath@users.noreply.github.com>
2021-02-21 10:33:14 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,72 @@
+from tkinter import *
+def Sentimental_analyse():
+    import string
+    from collections import Counter
+    import matplotlib.pyplot as plt
+
+    # reading text file
+    text = open("read.txt", encoding="utf-8").read()
+
+    # converting to lowercase
+    lower_case = text.lower()
+
+    # Removing punctuations
+    cleaned_text = lower_case.translate(str.maketrans('', '', string.punctuation))
+
+    # splitting text into words
+    tokenized_words = cleaned_text.split()
+
+    stop_words = ["i", "me", "my", "myself", "we", "our", "ours", "ourselves", "you", "your", "yours", "yourself",
+                  "yourselves", "he", "him", "his", "himself", "she", "her", "hers", "herself", "it", "its", "itself",
+                  "they", "them", "their", "theirs", "themselves", "what", "which", "who", "whom", "this", "that", "these",
+                  "those", "am", "is", "are", "was", "were", "be", "been", "being", "have", "has", "had", "having", "do",
+                  "does", "did", "doing", "a", "an", "the", "and", "but", "if", "or", "because", "as", "until", "while",
+                  "of", "at", "by", "for", "with", "about", "against", "between", "into", "through", "during", "before",
+                  "after", "above", "below", "to", "from", "up", "down", "in", "out", "on", "off", "over", "under", "again",
+                  "further", "then", "once", "here", "there", "when", "where", "why", "how", "all", "any", "both", "each",
+                  "few", "more", "most", "other", "some", "such", "no", "nor", "not", "only", "own", "same", "so", "than",
+                  "too", "very", "s", "t", "can", "will", "just", "don", "should", "now"]
+
+    # Removing stop words from the tokenized words list
+    final_words = []
+    for word in tokenized_words:
+        if word not in stop_words:
+            final_words.append(word)
+
+    emotion_list = []
+    with open('emotions.txt', 'r') as file:
+        for line in file:
+            clear_line = line.replace("\n", '').replace(",", '').replace("'", '').strip()
+            word, emotion = clear_line.split(':')
+
+            if word in final_words:
+                emotion_list.append(emotion)
+
+    #print(emotion_list)
+    w = Counter(emotion_list)
+    #print(w)
+
+    # Plotting the emotions on the graph
+
+    fig, ax1 = plt.subplots()
+    ax1.bar(w.keys(), w.values())
+    fig.autofmt_xdate()
+    plt.savefig('graph.png')
+    plt.show()
+
+def UI():
+
+    import tkinter as tk
+
+    root = tk.Tk()
+
+    canvas = tk.Canvas(root, height=600, width=500, bg="black")
+    label = Label(root, text='Welcome to Text Analyser', bg='black', fg='white', font="ar 15 ").pack(fill=BOTH)
+    canvas.pack()
+    # Buttons
+    chat = Button(root, text='Text Analyser', bg="White", fg='black', width=35,command=Sentimental_analyse)
+    chat.place(x=140, y=170)
+
+    root.mainloop()
+
+UI()
